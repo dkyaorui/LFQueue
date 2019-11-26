5cfa6e2d6d7debb820a0e22d349a34564a873ed9
moye <dkyaorui@163.com>
2019-11-27 00:41:05 +0800

    add banch mark test for push and pop

--- queue_test.go
+++ queue_test.go
@@ -44,24 +44,23 @@ func TestLFQueue_Push(t *testing.T) {
             var err *QueError
             err = que.Push(i)
             if err != nil {
-                if err.StatusCode == QueueIsFull{
+                if err.StatusCode == QueueIsFull {
                     fmt.Printf("err: %s, value: %d\n", err, i)
-                }else {
+                } else {
                     t.Errorf("que's push method run with wrong:%+v", err)
                 }
             }
         }(item)
     }
     wg.Wait()
-    fmt.Println(que.ringBuffer)
-    checkMap:= make(map[uint64]int)
+    checkMap := make(map[uint64]int)
     for index, value := range que.ringBuffer {
         if value.value == nil {
             continue
         }
         if checkMap[value.value.(uint64)] == 0 {
             checkMap[value.value.(uint64)] = index
-        }else{
+        } else {
             t.Errorf("data error occurs!index:%d, value:%d", index, value.value.(uint64))
         }
     }
@@ -70,10 +69,10 @@ func TestLFQueue_Push(t *testing.T) {
 func TestLFQueue_Pop(t *testing.T) {
     que := NewQue(inCapacity)
     var wg sync.WaitGroup
-    item := uint64(1)
-    for ; item <= 1024; item++ {
+    item := 1
+    for ; item <= inCapacity; item++ {
         wg.Add(1)
-        go func(item uint64) {
+        go func(item int) {
             defer wg.Done()
             err := que.Push(item)
             if err != nil {
@@ -85,25 +84,79 @@ func TestLFQueue_Pop(t *testing.T) {
     var checkMap sync.Map
     index := 0
     flag := true
-    for flag{
+    for flag {
         wg.Add(1)
-       go func(i int) {
-           defer wg.Done()
-           value, err := que.Pop()
-           if err != nil {
-               if err.StatusCode == QueueIsEmpty{
-                   flag = false
-                   return
-               }
-           }
-           _, ok := checkMap.Load(value.(uint64))
-           if ok {
-               t.Errorf("data error occurs!index:%d, value:%d", i, value.(uint64))
-           }else {
-               checkMap.Store(value.(uint64), 1)
-           }
-       }(index)
-       index++
+        go func(i int) {
+            defer wg.Done()
+            value, err := que.Pop()
+            if err != nil {
+                if err.StatusCode == QueueIsEmpty {
+                    flag = false
+                    return
+                }
+            }
+            _, ok := checkMap.Load(value.(int))
+            if ok {
+                t.Errorf("data error occurs!index:%d, value:%d", i, value.(int))
+            } else {
+                checkMap.Store(value.(int), 1)
+            }
+        }(index)
+        index++
+    }
+    wg.Wait()
+}
+
+func BenchmarkLFQueue_Push(b *testing.B) {
+    var wg sync.WaitGroup
+    que := NewQue(b.N)
+    capacity := uint64(b.N)
+    for item := uint64(1); item <= capacity; item++ {
+        wg.Add(1)
+        go func(i uint64) {
+            defer wg.Done()
+            var err *QueError
+            err = que.Push(i)
+            if err != nil {
+                if err.StatusCode == QueueIsFull {
+                    fmt.Printf("err: %s, value: %d\n", err, i)
+                } else {
+                    b.Errorf("que's push method run with wrong:%+v", err)
+                }
+            }
+        }(item)
+    }
+    wg.Wait()
+}
+
+func BenchmarkLFQueue_Pop(b *testing.B) {
+    b.StopTimer()
+    que := NewQue(b.N)
+    var wg sync.WaitGroup
+    item := 1
+    for ; item <= b.N; item++ {
+        wg.Add(1)
+        go func(item int) {
+            defer wg.Done()
+            err := que.Push(item)
+            if err != nil {
+                b.Errorf("que's push method run with wrong:%s", err)
+            }
+        }(item)
+    }
+    wg.Wait()
+    b.StartTimer()
+    for index := 0; index < b.N; index++ {
+        wg.Add(1)
+        go func(i int) {
+            defer wg.Done()
+            _, err := que.Pop()
+            if err != nil {
+                if err.StatusCode != QueueIsEmpty {
+                    b.Errorf("error: %+v", err)
+                }
+            }
+        }(index)
     }
     wg.Wait()
 }
